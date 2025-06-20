faaad2e10346ec81e8a25b0643aaa2350be2afc6
Jaemin Hong <hjm0901@gmail.com>
2025-06-20 13:39:55 +0000

    fn/static resolution

--- src/extern_resolver/type_resolver.rs
+++ src/extern_resolver/type_resolver.rs
@@ -1,6 +1,7 @@
+use etrace::some_or;
 use rustc_abi::VariantIdx;
 use rustc_hash::{FxHashMap, FxHashSet};
-use rustc_hir::{self as hir, intravisit};
+use rustc_hir::{self as hir, definitions::DefPathData, intravisit};
 use rustc_index::IndexVec;
 use rustc_middle::{
     hir::nested_filter,
@@ -18,9 +19,48 @@ impl Pass for TypeResolver {
 
     fn run(&self, tcx: TyCtxt<'_>) -> Self::Out {
         let result = resolve(tcx);
+        let ResolveResult {
+            equiv_adts: _equiv_adts,
+            equiv_fns: _equiv_fns,
+            unlinked_foreign_fns: _unlinked_foreign_fns,
+            equiv_statics: _equiv_statics,
+            unlinked_foreign_statics: _unlinked_foreign_statics,
+        } = result;
 
-        for (name, classes) in &result.equiv_adts {
-            if classes.0.len() > 1 {
+        // for (name, classes) in &result.equiv_adts {
+        //     if classes.0.len() > 1 {
+        //         println!("{name}");
+        //         for (id, def_ids) in classes.0.iter_enumerated() {
+        //             println!("  {id:?}");
+        //             for def_id in def_ids {
+        //                 println!("    {def_id:?}");
+        //             }
+        //         }
+        //     }
+        // }
+
+        // for (name, classes) in &_equiv_fns {
+        //     let unlinked = _unlinked_foreign_fns.get(name);
+        //     if unlinked.is_some() {
+        //         println!("{name}");
+        //         for (id, def_ids) in classes.0.iter_enumerated() {
+        //             println!("  {id:?}");
+        //             for def_id in def_ids {
+        //                 println!("    {def_id:?}");
+        //             }
+        //         }
+        //         if let Some(unlinked) = unlinked {
+        //             println!("  Unlinked");
+        //             for def_id in unlinked {
+        //                 println!("    {def_id:?}");
+        //             }
+        //         }
+        //     }
+        // }
+
+        for (name, classes) in &_equiv_statics {
+            let unlinked = _unlinked_foreign_statics.get(name);
+            if classes.0.len() > 1 || unlinked.is_some() {
                 println!("{name}");
                 for (id, def_ids) in classes.0.iter_enumerated() {
                     println!("  {id:?}");
@@ -28,49 +68,14 @@ impl Pass for TypeResolver {
                         println!("    {def_id:?}");
                     }
                 }
+                if let Some(unlinked) = unlinked {
+                    println!("  Unlinked");
+                    for def_id in unlinked {
+                        println!("    {def_id:?}");
+                    }
+                }
             }
         }
-
-        // let mut fns: FxHashMap<_, EquivClasses<LocalDefId>> = FxHashMap::default();
-        // for def_id in hir_data.fns {
-        //     let name = ir_util::def_id_to_value_symbol(def_id, tcx).unwrap();
-        //     let classes = fns.entry(name).or_insert_with(EquivClasses::new);
-        //     classes.insert(def_id, |id1, id2| {
-        //         let sig1 = tcx.fn_sig(*id1).skip_binder().skip_binder();
-        //         let sig2 = tcx.fn_sig(*id2).skip_binder().skip_binder();
-        //         let inputs1 = sig1.inputs();
-        //         let inputs2 = sig2.inputs();
-        //         if sig1.c_variadic != sig2.c_variadic
-        //             || sig1.safety != sig2.safety
-        //             || sig1.abi != sig2.abi
-        //             || inputs1.len() != inputs2.len()
-        //         {
-        //             return false;
-        //         }
-        //         for (ty1, ty2) in inputs1.iter().zip(inputs2) {
-        //             if !cmp.cmp_tys(*ty1, *ty2, None, &FxHashSet::default()) {
-        //                 return false;
-        //             }
-        //         }
-        //         cmp.cmp_tys(sig1.output(), sig2.output(), None, &FxHashSet::default())
-        //     });
-        // }
-
-        // let mut statics: FxHashMap<_, EquivClasses<LocalDefId>> = FxHashMap::default();
-        // for def_id in hir_data.statics {
-        //     let name = ir_util::def_id_to_value_symbol(def_id, tcx).unwrap();
-        //     let classes = statics.entry(name).or_insert_with(EquivClasses::new);
-        //     classes.insert(def_id, |id1, id2| {
-        //         let ty1 = tcx.type_of(*id1).skip_binder();
-        //         let ty2 = tcx.type_of(*id2).skip_binder();
-        //         if !cmp.cmp_tys(ty1, ty2, None, &FxHashSet::default()) {
-        //             return false;
-        //         }
-        //         let init1 = tcx.eval_static_initializer(*id1);
-        //         let init2 = tcx.eval_static_initializer(*id2);
-        //         init1 == init2
-        //     });
-        // }
 
         // let mut tys: FxHashMap<_, EquivClasses<LocalDefId>> = FxHashMap::default();
         // for def_id in hir_data.tys {
@@ -82,57 +87,15 @@ impl Pass for TypeResolver {
         //         cmp.cmp_tys(ty1, ty2, None, &FxHashSet::default())
         //     });
         // }
-
-        // for equiv_class in scc_classes.0 {
-        //     for scc_id in equiv_class {
-        //         let scc = &sccs.sccs[scc_id];
-        //         println!("{scc:?}");
-        //     }
-        //     println!();
-        // }
-
-        // for unnamed in cmp.equiv_unnameds.iter() {
-        //     let rep = cmp.equiv_unnameds.find_set(unnamed);
-        //     println!("{rep:?} = {unnamed:?}");
-        // }
-
-        // for (name, equiv_classes) in fns {
-        //     println!("{name}");
-        //     for equiv_class in equiv_classes.0 {
-        //         for def_id in equiv_class {
-        //             println!("  {def_id:?}");
-        //         }
-        //         println!();
-        //     }
-        //     println!();
-        // }
-
-        // for (name, equiv_classes) in tys {
-        //     println!("{name}");
-        //     for equiv_class in equiv_classes.0 {
-        //         for def_id in equiv_class {
-        //             println!("  {def_id:?}");
-        //         }
-        //         println!();
-        //     }
-        //     println!();
-        // }
-
-        // for (name, equiv_classes) in statics {
-        //     println!("{name}");
-        //     for equiv_class in equiv_classes.0 {
-        //         for def_id in equiv_class {
-        //             println!("  {def_id:?}");
-        //         }
-        //         println!();
-        //     }
-        //     println!();
-        // }
     }
 }
 
 struct ResolveResult {
     equiv_adts: FxHashMap<Symbol, EquivClasses<LocalDefId>>,
+    equiv_fns: FxHashMap<Symbol, EquivClasses<LocalDefId>>,
+    unlinked_foreign_fns: FxHashMap<Symbol, Vec<(LocalDefId, Vec<EquivClassId>)>>,
+    equiv_statics: FxHashMap<Symbol, EquivClasses<LocalDefId>>,
+    unlinked_foreign_statics: FxHashMap<Symbol, Vec<(LocalDefId, Vec<EquivClassId>)>>,
 }
 
 fn resolve(tcx: TyCtxt<'_>) -> ResolveResult {
@@ -197,7 +160,144 @@ fn resolve(tcx: TyCtxt<'_>) -> ResolveResult {
         }
     }
 
-    ResolveResult { equiv_adts }
+    let mut equiv_fns: FxHashMap<_, EquivClasses<LocalDefId>> = FxHashMap::default();
+    for def_id in hir_data.fns {
+        let name = ir_util::def_id_to_value_symbol(def_id, tcx).unwrap();
+        let name_str = name.as_str();
+        if name_str == "main" {
+            continue;
+        }
+        let classes = equiv_fns.entry(name).or_insert_with(EquivClasses::new);
+        classes.insert(def_id, |id1, id2| {
+            let hir::Node::Item(item1) = tcx.hir_node_by_def_id(*id1) else { panic!() };
+            let hir::Node::Item(item2) = tcx.hir_node_by_def_id(*id2) else { panic!() };
+            let span1 = item1.span;
+            let span2 = item2.span;
+            let len1 = span1.hi() - span1.lo();
+            let len2 = span2.hi() - span2.lo();
+            len1 == len2 && cmp.cmp_fn_sigs(*id1, *id2)
+        });
+    }
+    let mut unlinked_foreign_fns: FxHashMap<_, Vec<_>> = FxHashMap::default();
+    for def_id in hir_data.foreign_fns {
+        let name = ir_util::def_id_to_value_symbol(def_id, tcx).unwrap();
+        let classes = some_or!(equiv_fns.get_mut(&name), continue);
+        let mut link_candidates: Vec<_> = classes
+            .0
+            .iter_enumerated()
+            .filter_map(|(id, class)| {
+                if cmp.cmp_fn_sigs(class[0], def_id) {
+                    Some(id)
+                } else {
+                    None
+                }
+            })
+            .collect();
+        filter_by_common_def_path(&mut link_candidates, def_id, classes, tcx);
+        if let [i] = link_candidates[..] {
+            classes.0[i].push(def_id);
+        } else {
+            unlinked_foreign_fns
+                .entry(name)
+                .or_default()
+                .push((def_id, link_candidates));
+        }
+    }
+
+    let mut equiv_statics: FxHashMap<_, EquivClasses<LocalDefId>> = FxHashMap::default();
+    for def_id in hir_data.statics {
+        let name = ir_util::def_id_to_value_symbol(def_id, tcx).unwrap();
+        let classes = equiv_statics.entry(name).or_insert_with(EquivClasses::new);
+        classes.insert(def_id, |id1, id2| {
+            if !cmp.cmp_type_of(*id1, *id2) {
+                return false;
+            }
+            let init1 = tcx.eval_static_initializer(*id1);
+            let init2 = tcx.eval_static_initializer(*id2);
+            init1 == init2
+        });
+    }
+    let mut unlinked_foreign_statics: FxHashMap<_, Vec<_>> = FxHashMap::default();
+    for def_id in hir_data.foreign_statics {
+        let name = ir_util::def_id_to_value_symbol(def_id, tcx).unwrap();
+        let classes = some_or!(equiv_statics.get_mut(&name), continue);
+        let mut link_candidates: Vec<_> = classes
+            .0
+            .iter_enumerated()
+            .filter_map(|(id, class)| {
+                if cmp.cmp_type_of(class[0], def_id) {
+                    Some(id)
+                } else {
+                    None
+                }
+            })
+            .collect();
+        filter_by_common_def_path(&mut link_candidates, def_id, classes, tcx);
+        if let [i] = link_candidates[..] {
+            classes.0[i].push(def_id);
+        } else {
+            unlinked_foreign_statics
+                .entry(name)
+                .or_default()
+                .push((def_id, link_candidates));
+        }
+    }
+
+    ResolveResult {
+        equiv_adts,
+        equiv_fns,
+        unlinked_foreign_fns,
+        equiv_statics,
+        unlinked_foreign_statics,
+    }
+}
+
+fn filter_by_common_def_path(
+    candidates: &mut Vec<EquivClassId>,
+    def_id: LocalDefId,
+    classes: &EquivClasses<LocalDefId>,
+    tcx: TyCtxt<'_>,
+) {
+    if candidates.len() > 1 {
+        let common_lengths: Vec<_> = candidates
+            .iter()
+            .map(|id| {
+                let class = &classes.0[*id];
+                let max = class
+                    .iter()
+                    .map(|def_id0| common_def_path_len(def_id, *def_id0, tcx))
+                    .max()
+                    .unwrap();
+                (*id, max)
+            })
+            .collect();
+        candidates.clear();
+        let mut max = 0;
+        for (i, len) in common_lengths {
+            if len > max {
+                max = len;
+                candidates.clear();
+                candidates.push(i);
+            } else if len == max {
+                candidates.push(i);
+            }
+        }
+    }
+}
+
+fn common_def_path_len(def_id1: LocalDefId, def_id2: LocalDefId, tcx: TyCtxt<'_>) -> usize {
+    let def_path1 = tcx.def_path(def_id1.to_def_id());
+    let def_path2 = tcx.def_path(def_id2.to_def_id());
+    def_path1
+        .data
+        .into_iter()
+        .zip(def_path2.data)
+        .take_while(|(data1, data2)| {
+            let DefPathData::TypeNs(name1) = data1.data else { panic!() };
+            let DefPathData::TypeNs(name2) = data2.data else { panic!() };
+            name1 == name2
+        })
+        .count()
 }
 
 struct TypeComparator<'a, 'tcx> {
@@ -323,9 +423,9 @@ impl<'tcx> TypeComparator<'_, 'tcx> {
                 }
                 _ => false,
             },
-            Array(ty1, len1) => {
-                let Array(ty2, len2) = ty2_kind else { return false };
-                len1 == len2 && self.cmp_tys(*ty1, *ty2)
+            Array(ty1, _) => {
+                let Array(ty2, _) = ty2_kind else { return false };
+                self.cmp_tys(*ty1, *ty2)
             }
             RawPtr(ty1, m1) => {
                 let RawPtr(ty2, m2) = ty2_kind else { return false };
@@ -360,6 +460,32 @@ impl<'tcx> TypeComparator<'_, 'tcx> {
             }
             _ => panic!("{ty1_kind:?}"),
         }
+    }
+
+    fn cmp_fn_sigs(&mut self, def_id1: LocalDefId, def_id2: LocalDefId) -> bool {
+        let sig1 = self.tcx.fn_sig(def_id1).skip_binder().skip_binder();
+        let sig2 = self.tcx.fn_sig(def_id2).skip_binder().skip_binder();
+        let inputs1 = sig1.inputs();
+        let inputs2 = sig2.inputs();
+        if sig1.c_variadic != sig2.c_variadic
+            || sig1.safety != sig2.safety
+            || sig1.abi != sig2.abi
+            || inputs1.len() != inputs2.len()
+        {
+            return false;
+        }
+        for (ty1, ty2) in inputs1.iter().zip(inputs2) {
+            if !self.cmp_tys(*ty1, *ty2) {
+                return false;
+            }
+        }
+        self.cmp_tys(sig1.output(), sig2.output())
+    }
+
+    fn cmp_type_of(&mut self, def_id1: LocalDefId, def_id2: LocalDefId) -> bool {
+        let ty1 = self.tcx.type_of(def_id1).skip_binder();
+        let ty2 = self.tcx.type_of(def_id2).skip_binder();
+        self.cmp_tys(ty1, ty2)
     }
 }
 
@@ -417,6 +543,7 @@ impl<T> EquivClasses<T> {
         Self(IndexVec::new())
     }
 
+    /// Inserts a value into the proper equivalence class.
     #[inline]
     fn insert<F: FnMut(&T, &T) -> bool>(&mut self, v: T, mut cmp: F) {
         for equiv_class in &mut self.0 {
@@ -483,20 +610,22 @@ impl<'tcx> intravisit::Visitor<'tcx> for HirVisitor<'tcx> {
 
     fn visit_item(&mut self, item: &'tcx hir::Item<'tcx>) -> Self::Result {
         let def_id = item.owner_id.def_id;
-        match item.kind {
-            hir::ItemKind::Fn { .. } => {
-                self.data.fns.push(def_id);
+        if self.tcx.visibility(def_id).is_public() {
+            match item.kind {
+                hir::ItemKind::Fn { .. } => {
+                    self.data.fns.push(def_id);
+                }
+                hir::ItemKind::Static { .. } => {
+                    self.data.statics.push(def_id);
+                }
+                hir::ItemKind::Struct(_, _, _) | hir::ItemKind::Union(_, _, _) => {
+                    self.data.adts.push(def_id);
+                }
+                hir::ItemKind::TyAlias(_, _, _) => {
+                    self.data.tys.push(def_id);
+                }
+                _ => {}
             }
-            hir::ItemKind::Static { .. } => {
-                self.data.statics.push(def_id);
-            }
-            hir::ItemKind::Struct(_, _, _) | hir::ItemKind::Union(_, _, _) => {
-                self.data.adts.push(def_id);
-            }
-            hir::ItemKind::TyAlias(_, _, _) => {
-                self.data.tys.push(def_id);
-            }
-            _ => {}
         }
 
         intravisit::walk_item(self, item)
