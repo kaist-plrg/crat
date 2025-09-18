6be523208f9ca05f3e27f909ccd5b616ae966e8b
Ryan Lee <minwook.lee@pm.me>
2025-09-18 06:41:02 +0000

    return,calls

--- src/finder/enum_finder/mir/basic_block.rs
+++ src/finder/enum_finder/mir/basic_block.rs
@@ -18,14 +18,14 @@ pub struct PlaceAnalyze<'tcx> {
 pub fn process_basic_blocks(
     body_idx: BodyIndex,
     basic_blocks: &BasicBlocks<'_>,
-    variables: &PairIndexVec<IdentifierKey, IdentifierDetail<'_>>,
-    tcx: TyCtxt<'_>,
-    enum_tys: &[EnumTy],
-    enum_usages: &[EnumTyAnnotation<'_>],
+    _variables: &PairIndexVec<IdentifierKey, IdentifierDetail<'_>>,
+    _tcx: TyCtxt<'_>,
+    _enum_tys: &[EnumTy],
+    _enum_usages: &[EnumTyAnnotation<'_>],
 ) {
-    let assigns = basic_blocks
-        .iter_enumerated()
-        .flat_map(|(i, block_data)| {
+    let _assigns = basic_blocks
+        .iter()
+        .flat_map(|block_data| {
             block_data.statements.iter().filter_map(move |stmt| {
                 if let StatementKind::Assign(box (place, rvalue)) = &stmt.kind {
                     Some((
@@ -42,19 +42,57 @@ pub fn process_basic_blocks(
         })
         .collect::<Vec<_>>();
 
-    for assign in &assigns {
-        let place_analyze = &assign.0;
-        let rvalue = &assign.1;
+    // for assign in &assigns {
+    //     let place_analyze = &assign.0;
+    //     let rvalue = &assign.1;
 
-        if let Some(var_detail) = variables.get(place_analyze.identifier) {
-            println!(
-                "BodyIndex: {:?}\tLocal: {:?}\tProjection: {:?}\tRvalue: {:?}\tSource Info: {:?}",
-                place_analyze.identifier.0,
-                place_analyze.identifier.1,
-                place_analyze.projection,
-                rvalue,
-                var_detail.source_info.span
-            );
+    //     if let Some(var_detail) = variables.get(place_analyze.identifier) {
+    //         println!(
+    //             "BodyIndex: {:?}\tLocal: {:?}\tProjection: {:?}\tRvalue: {:?}\tSource Info: {:?}",
+    //             place_analyze.identifier.0,
+    //             place_analyze.identifier.1,
+    //             place_analyze.projection,
+    //             rvalue,
+    //             var_detail.source_info.span
+    //         );
+    //     }
+    // }
+
+    let _returns = basic_blocks
+        .iter()
+        .filter_map(|block_data| {
+            block_data.terminator.as_ref().filter(|terminator| {
+                matches!(terminator.kind, rustc_middle::mir::TerminatorKind::Return)
+            })
+        })
+        .collect::<Vec<_>>();
+
+    // for ret in &returns {
+    //     println!("BodyIndex: {body_idx:?}\tReturn Terminator: {ret:?}");
+    // }
+
+    let calls = basic_blocks
+        .iter()
+        .filter_map(|block_data| {
+            block_data.terminator.as_ref().filter(|terminator| {
+                matches!(
+                    terminator.kind,
+                    rustc_middle::mir::TerminatorKind::Call { .. }
+                        | rustc_middle::mir::TerminatorKind::TailCall { .. }
+                )
+            })
+        })
+        .collect::<Vec<_>>();
+
+    let mut tail_call_cnt = 0;
+    for call in &calls {
+        if matches!(
+            call.kind,
+            rustc_middle::mir::TerminatorKind::TailCall { .. }
+        ) {
+            tail_call_cnt += 1;
         }
+        println!("BodyIndex: {body_idx:?}\tCall Terminator: {call:?}");
     }
+    println!("Tail Call Count: {tail_call_cnt}"); // prints 0
 }
