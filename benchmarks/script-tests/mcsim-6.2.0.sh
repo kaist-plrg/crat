590f327b52be38bdc1da9eb45fdda5715a89abe8
Jaemin Hong <hjm0901@gmail.com>
2025-10-14 10:08:02 +0000

    fix mcsim test script

--- benchmarks/script-tests/mcsim-6.2.0.sh
+++ benchmarks/script-tests/mcsim-6.2.0.sh
@@ -6,7 +6,8 @@ script_dir="$(dirname "$0")"
 . "$script_dir/prepare"
 
 mkdir "$1/$name/mod"
-cp "$2/$name/target/$dir/mod_mod_main" "$1/$name/mod/mod"
+cp "$2/$name/target/$dir/mod_mod_main" "$1/$name/mod/mod" || \
+cp "$2/$name/target/$dir/rmod_rmod_main" "$1/$name/mod/mod"
 
 cd "$1/$name"
 make check-TESTS
